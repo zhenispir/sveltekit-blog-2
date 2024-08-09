cf9b59a3cd40e932fd70524215c38e41f79841e7
Zhenis Pir <yenispiro@gmail.com>
2024-08-09 18:45:01 +0500

    Came to the part of Syntax Highlighting

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment'
 
-export const title = 'Shakespeare'
+export const title = 'Zhenis Pir'
 export const description = 'SvelteKit blog for poets'
 export const url = dev ? 'http://localhost:5173/' : 'https://zhenispir.com/'
